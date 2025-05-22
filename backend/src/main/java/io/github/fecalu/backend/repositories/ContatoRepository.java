5106b0f25b7326949b2b24de1869585507f242d6
devopMarkz <marcosdev2002@gmail.com>
2025-05-22 17:33:46 -0300

    Criacao de metodo para verificar a existencia de usuarios com contato especifico

--- backend/src/main/java/io/github/fecalu/backend/repositories/ContatoRepository.java
+++ backend/src/main/java/io/github/fecalu/backend/repositories/ContatoRepository.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ContatoRepository extends JpaRepository<Contato, Long> {
+
+    boolean existsByNumeroTelefone(String numeroTelefone);
+
 }
